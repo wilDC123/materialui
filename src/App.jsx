3e8c6bc3ffd9d83979b1be70fa203e5c2577f6a5
wilDC123 <diaswil521@gmail.com>
2024-09-18 01:03:42 -0400

    Materialui

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+//import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
+export default function App(){
+  return(
+    <>
+    <h1>App</h1>
+    <Button variant="contained">Mi primer boton</Button>
+    </>
+  );
+}
